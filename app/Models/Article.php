8ffa849c389c8f2a110d261d1229f5f82125a2fe
babamurad <babamurad2010@yandex.ru>
2024-07-09 00:09:48 +0500

    Article model

---
+++ app/Models/Article.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'content', 'image', 'published', 'publish_date', 'order', 'author'];
+
+    public function scopePublished($query)
+    {
+        return $query->where('published', true);
+    }
+}
