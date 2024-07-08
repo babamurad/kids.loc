3a8de67b0ccaf7f2c5eb621527e2fa5a6fda1b80
babamurad <babamurad2010@yandex.ru>
2024-07-09 00:15:01 +0500

    Article Index

---
+++ app/Livewire/Admin/Article/ArticleEditComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Article;
+
+use Livewire\Component;
+
+class ArticleEditComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.article.article-edit-component');
+    }
+}
---
+++ app/Livewire/Admin/Article/ArticleIndexComponent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Livewire\Admin\Article;
+
+use App\Models\Article;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ArticleIndexComponent extends Component
+{
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
+    public $del_id;
+    public $image;
+
+    public function render()
+    {
+        return view('livewire.admin.article.article-index-component')
+            ->layout('components.layouts.admin-app');
+    }
+
+    public function deleteId($id)
+    {
+        $this->del_id = $id;
+    }
+
+    public function destroy()
+    {
+        $article = Article::findOrFail($this->del_id);
+        $this->image = $article->image;
+        if (file_exists('images/teachers/'.$this->image)){
+            unlink('images/teachers/'.$this->image);
+        }
+        $article->delete();
+
+        $this->dispatch('closeModal');
+        session()->flash('error', 'Deleted.');
+    }
+
+    public function cancel()
+    {
+        $this->del_id = '';
+    }
+}
